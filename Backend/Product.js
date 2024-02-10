19d933033b20a499f8b1a68b7b2bffcf74c10bf0
PRANJAL2903 <84214223+PRANJAL2903@users.noreply.github.com>
2024-02-10 12:05:18 +0530

    Add files via upload

---
+++ Backend/Product.js
@@ -0,0 +1,12 @@
+// backend/models/Product.js
+
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    category: String,
+    price: Number,
+    stock: Number
+});
+
+module.exports = mongoose.model('Product', productSchema);
